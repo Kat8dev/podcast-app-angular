06c407af47c91ab3f6ffbdc4e50ae2633521460c
Khatuna <you@example.com>
2023-08-15 22:04:56 +0200

    environments prod

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   podcastList: 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json',
   podcastDetails:
     'https://itunes.apple.com/lookup'
